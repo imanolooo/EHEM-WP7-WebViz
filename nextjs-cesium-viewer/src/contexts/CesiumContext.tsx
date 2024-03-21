a19a2a7f8a422a0ec562bda0a062e81ed6448b3d
eavraam <e_avraam@outlook.com>
2024-03-21 13:39:09 +0200

    Cesium camera now flies to the 'goto' information fetched. Refactored code structure, since my initial idea with useContext was getting too complex to figure out.

--- nextjs-cesium-viewer/src/contexts/CesiumContext.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CesiumContext = React.createContext({
-  setCameraView: (viewConfig: any) => {
-    console.log('setCameraView not implemented');
-  },
-});
-
-export default CesiumContext;
