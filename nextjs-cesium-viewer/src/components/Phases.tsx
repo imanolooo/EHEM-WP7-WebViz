514171b44d7ddbb18f257bfdb8cea50f65ff94d4
Imanol Munoz-Pandiella <imanol.munoz.pandiella@gmail.com>
2024-04-16 00:05:32 +0200

    solved again the text of the button

--- nextjs-cesium-viewer/src/components/Phases.tsx
+++ nextjs-cesium-viewer/src/components/Phases.tsx
@@ -23,12 +23,12 @@ export const phasesInfo = [
     { id: 2504190, text: "Phase XIII" },
     // 3D Tilset -> NO TILESET
     { id: 2533920, text: "Phase XXI" }*/
-    { id: 2536536, text: "Phase IX" },
-    { id: 2536537, text: "Phase X" },
-    { id: 2536538, text: "Phase XI" },
-    { id: 2536539, text: "Phase XII" },
-    { id: 2536540, text: "Phase XIII" },
-    { id: 2536602, text: "Phase XXI" }
+    { id: 2536536, text: "Phase IXth cent." },
+    { id: 2536537, text: "Phase Xth cent." },
+    { id: 2536538, text: "Phase XIth cent." },
+    { id: 2536539, text: "Phase XIIth cent." },
+    { id: 2536540, text: "Phase XIIIth cent." },
+    { id: 2536602, text: "Phase XXIth cent." }
 ];
 
 
